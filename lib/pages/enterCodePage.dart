100800b6632ce1663a29f805ae8216f94764ca3e
Ulysses Uribe <ulysses.uribe@gmail.com>
2020-10-01 22:28:06 -0700

    added loading screen between enterCodePage and mapRender

--- lib/pages/enterCodePage.dart
+++ lib/pages/enterCodePage.dart
@@ -1,6 +1,7 @@
 // 8-4, the enter code page
 
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../globalVar.dart';
 import '../appBar.dart';
@@ -8,6 +9,8 @@ import 'firebaseFunctions.dart';
 import 'firebaseFunctions.dart';
 import 'mapRenderPage.dart';
 import 'package:flutter_fadein/flutter_fadein.dart';
+
+bool loadingScreen = false;
 
 class Page4 extends StatefulWidget {
   final String roomCode;
@@ -65,6 +68,7 @@ class _Page4State extends State<Page4> {
   void initState() {
     super.initState();
     _fadeInFunc();
+    loadingScreen = false;
   }
 
   @override
@@ -74,126 +78,140 @@ class _Page4State extends State<Page4> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBarMain(context),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Center(
-            child: Column(children: <Widget>[
-              FadeIn(
-                controller: textController,
-                child: Container(
-                  padding: EdgeInsets.fromLTRB(
-                      0, MediaQuery.of(context).size.height * 0.25, 0, 20),
-                  child: Text(
-                    "Enter Code Below:",
-                    style: GoogleFonts.roboto(fontSize: 25),
-                  ),
-                ),
+    return (loadingScreen == true)
+        ? Container(
+            decoration: BoxDecoration(
+              color: Color(Global.backgroundColor),
+            ),
+            child: Center(
+              child: SpinKitPulse(
+                size: 280,
+                color: Colors.black38,
               ),
-              FadeIn(
-                controller: codeAnimController,
-                child: Container(
-                  margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
-                  width: 200.0,
-                  child: Form(
-                    key: formKey,
-                    child: TextFormField(
-                      maxLength: 5,
-                      decoration: InputDecoration(
-                        hintText: 'e.g. abcde',
-                        hintStyle: GoogleFonts.roboto(fontSize: 18),
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
+            ),
+          )
+        : Scaffold(
+            appBar: appBarMain(context),
+            body: SafeArea(
+              child: SingleChildScrollView(
+                child: Center(
+                  child: Column(children: <Widget>[
+                    FadeIn(
+                      controller: textController,
+                      child: Container(
+                        padding: EdgeInsets.fromLTRB(0,
+                            MediaQuery.of(context).size.height * 0.25, 0, 20),
+                        child: Text(
+                          "Enter Code Below:",
+                          style: GoogleFonts.roboto(fontSize: 25),
                         ),
                       ),
-                      validator: (val) {
-                        if (val.isEmpty) {
-                          return "Please enter a code";
-                        }
-                        if (val.length != 5) {
-                          return "Must be 5 characters long";
-                        }
-                        if (isValid == 'false') {
-                          return (this.validationMessage);
-                        }
-                        return null;
-                      },
-                      onChanged: (text) {
-                        String msg;
-                        if (text == "") {
-                          msg = "Enter a valid code";
-                        }
-                        roomCode = text;
-                        this.setState(() {
-                          roomCode = text;
-                          validationMessage = msg;
-                        });
-                      },
-                      controller: codeController,
                     ),
-                  ),
-                ),
-              ),
-              FadeIn(
-                controller: buttonController,
-                child: Container(
-                  height: 50,
-                  width: 100,
-                  child: RaisedButton(
-                    color: Colors.black54,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                      //side: BorderSide(color: Colors.grey),
-                    ),
-                    child: Text(
-                      "Go",
-                      style: GoogleFonts.roboto(
-                        fontSize: 20,
-                        color: Colors.white,
+                    FadeIn(
+                      controller: codeAnimController,
+                      child: Container(
+                        margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                        width: 200.0,
+                        child: Form(
+                          key: formKey,
+                          child: TextFormField(
+                            maxLength: 5,
+                            decoration: InputDecoration(
+                              hintText: 'e.g. abcde',
+                              hintStyle: GoogleFonts.roboto(fontSize: 18),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey),
+                              ),
+                            ),
+                            validator: (val) {
+                              if (val.isEmpty) {
+                                return "Please enter a code";
+                              }
+                              if (val.length != 5) {
+                                return "Must be 5 characters long";
+                              }
+                              if (isValid == 'false') {
+                                return (this.validationMessage);
+                              }
+                              return null;
+                            },
+                            onChanged: (text) {
+                              String msg;
+                              if (text == "") {
+                                msg = "Enter a valid code";
+                              }
+                              roomCode = text;
+                              this.setState(() {
+                                roomCode = text;
+                                validationMessage = msg;
+                              });
+                            },
+                            controller: codeController,
+                          ),
+                        ),
                       ),
                     ),
-                    // When the user presses the button, show an alert dialog containing
-                    // the text that the user has entered into the text field.
-                    onPressed: () async {
-                      if (formKey.currentState.validate()) {
-                        setState(() {
-                          isLoading = true;
-                        });
+                    FadeIn(
+                      controller: buttonController,
+                      child: Container(
+                        height: 50,
+                        width: 100,
+                        child: RaisedButton(
+                          color: Colors.black54,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                            //side: BorderSide(color: Colors.grey),
+                          ),
+                          child: Text(
+                            "Go",
+                            style: GoogleFonts.roboto(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                          // When the user presses the button, show an alert dialog containing
+                          // the text that the user has entered into the text field.
+                          onPressed: () async {
+                            if (formKey.currentState.validate()) {
+                              setState(() {
+                                loadingScreen = true;
+                              });
 
-                        bool isSuccess =
-                            await FirebaseFunctions.addCurrentUserToRoom(
-                                this.roomCode);
-                        if (isSuccess) {
-                          await FirebaseFunctions.refreshFirebaseUserData();
-                          await FirebaseFunctions.refreshFirebaseRoomData();
+                              bool isSuccess =
+                                  await FirebaseFunctions.addCurrentUserToRoom(
+                                      this.roomCode);
+                              if (isSuccess) {
+                                await FirebaseFunctions
+                                    .refreshFirebaseUserData();
+                                await FirebaseFunctions
+                                    .refreshFirebaseRoomData();
 //              sendToRoom();
-                          _sendDataToPage3(context);
-                        } else {
-                          setState(() {
-                            isLoading = false;
-                            isValid = 'false';
-                            validationMessage = "Invalid code entered";
-                            print('code not valid');
-                          });
-                          this.setState(() {
-                            validationMessage = "Invalid code entered";
-                          });
-                        }
-                      }
-                    },
-                    // to show Go text in button
-                  ),
+                                _sendDataToPage3(context);
+                              } else {
+                                setState(() {
+                                  loadingScreen = false;
+                                  isValid = 'false';
+                                  validationMessage = "Invalid code entered";
+                                  print('code not valid');
+                                });
+                                this.setState(() {
+                                  validationMessage = "Invalid code entered";
+                                });
+                              }
+                            }
+                          },
+                          // to show Go text in button
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 30)
+                  ]),
                 ),
               ),
-              SizedBox(height: 30)
-            ]),
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
